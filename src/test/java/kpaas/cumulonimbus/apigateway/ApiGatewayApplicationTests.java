dc647d798c2f9a0f89675e5d5af383090d493c8c
SangWookie <swcube520@gmail.com>
2024-08-03 22:17:31 +0900

    initial commit

---
+++ src/test/java/kpaas/cumulonimbus/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kpaas.cumulonimbus.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
